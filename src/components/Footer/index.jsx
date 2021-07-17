d723123fe53bc234c9dd1d4b145d8dd274350ee8
denhur62 <denhur62@naver.com>
2021-07-17 22:50:43 +0900

    modiyeid profile

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,19 @@
+import './index.scss';
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://github.com/ddongule/ddongule-simple-portfolio'
+      >
+        
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
