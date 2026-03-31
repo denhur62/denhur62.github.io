2589edf79863ad712fbc479c08eb4bc59eeae966
denhur62 <denhur62@naver.com>
2026-04-01 01:42:54 +0900

    최종 수정

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,21 +1,9 @@
-import './index.scss';
+import "./index.scss";
 
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div className='footer-container'>
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://github.com/ddongule/ddongule-simple-portfolio'
-      >
-        <p className='footer-contents'>
-          <span className='icon'>&copy; 2021</span> ddongule-simple-portfolio
-        </p>
-      </a>
-    </div>
-  );
+  return <div className="footer-container"></div>;
 };
 
 export default Footer;
